883a849e6278d2d8487b7db4b9634117748ffc56
ASahinoglu <aydinsahinoglu888@gmail.com>
2025-07-10 15:41:47 +0200

    solved task 4522

--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_2.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_2.dart
@@ -4,6 +4,28 @@ class S4522 extends StatelessWidget {
   const S4522({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return MyStatelessWidget();
+  }
+}
+
+class MyStatelessWidget extends StatelessWidget {
+  const MyStatelessWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        const Text(
+          'Hello Aydin',
+          style: TextStyle(fontSize: 24),
+        ),
+        const SizedBox(height: 12),
+        ElevatedButton(
+          onPressed: () {},
+          child: const Text('Klick mich'),
+        ),
+      ],
+    );
   }
 }
